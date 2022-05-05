6971568f4c22c45e059e060472ece8219f8277fc
Ramindu Nanayakkara <44117883+raminduns@users.noreply.github.com>
2022-05-05 11:43:42 +0530

    Add negative tests

---
+++ tests/negative-tests/PUT.negative.test.js
@@ -0,0 +1,23 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const logger = require('../../logger/logger');
+
+test.beforeEach(async ({}, testInfo) => {
+    logger.info(`Worker ${testInfo.workerIndex} - Running ${testInfo.title}`);
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+    logger.info(`Worker ${testInfo.workerIndex} - Finished ${testInfo.title} with status ${testInfo.status}`);
+  
+    if (testInfo.status !== testInfo.expectedStatus)
+        logger.info(`Worker ${testInfo.workerIndex} - Did not run as expected, ended up at ${page.url()}`);
+
+    if(testInfo.config.workers==1)    
+        logger.info("-------------------------------------------------------"); 
+});
+
+test('when the request used an incorrect method - PUT', async ({ request },testInfo) => {
+    const response = await request.put('/cad.api',{});
+    expect(response.status()).toBe(405);
+    logger.info(await response.json());
+});
